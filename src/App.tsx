235843c73f9e250e3c62ba1827d90806e50324ee
Ashish <ashishkhadka317@gmail.com>
2026-05-13 12:59:18 +0545

    "added navbar component"

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import  Signup  from "@/components/examples/dialog/standard/dialog-signup-form";
 import  Signin  from "@/components/examples/dialog/standard/dialog-signin-form";
 
 
+
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
