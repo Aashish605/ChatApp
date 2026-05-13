3acaa111492f1d61b63b7bcdf4ad04f37ddbe9d2
Arjun Adhikari <arjun610705@gmail.com>
2026-05-13 12:56:53 +0545

    configure route in router.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import { Button } from "@/components/ui/button"
-import  Signup  from "@/components/examples/dialog/standard/dialog-signup-form";
-import  Signin  from "@/components/examples/dialog/standard/dialog-signin-form";
-
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-      <Signup />
-      <Signin />
-    </div>
+    <>
+    <h1>This is h1</h1></>
   )
 }
 
