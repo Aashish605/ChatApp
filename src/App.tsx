e7c4f33dde6f7cbf9b52902e85262cd1e6d2d72c
Ashish <ashishkhadka317@gmail.com>
2026-05-13 12:43:50 +0545

    added the login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button"
 import  Signup  from "@/components/examples/dialog/standard/dialog-signup-form";
+import  Signin  from "@/components/examples/dialog/standard/dialog-signin-form";
+
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
       <Signup />
+      <Signin />
     </div>
   )
 }
